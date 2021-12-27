4a1c3aa575ecaebb2f5584a22d442530ff4dd931
Sulaymon Olimov <Sulaymon_Olimov@epam.com>
2021-12-27 21:35:33 +0500

    added: Editing functionality

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const todosWrapper = document.querySelector(".todos__wrapper");
 const todoForm = document.querySelector(".todo__add__form form");
 const todoInput = document.querySelector(".todo__add__form form .todo__input");
+// Edit Modal
+const editModal = document.querySelector(".edit__modal");
+const todoEditForm = document.querySelector(".edit__modal form");
+const todoEditInput = document.querySelector(
+  ".edit__modal form .edit__todo__input"
+);
 
 const state = [];
 /*
@@ -30,6 +36,22 @@ todoForm.addEventListener("submit", function (event) {
   addTodoToState(todoText);
 });
 
+todoEditForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const todoText = todoEditInput.value.trim();
+  // todoId string bo'lgani uchun + qoyib number tipiga o'tkazdim
+  const todoId = +todoEditInput.dataset.todoId;
+  todoInput.value = "";
+  event.preventDefault();
+  // validation
+  if (!todoText) {
+    alert("Tog'ri qiymat kiriting!!!");
+    return;
+  }
+
+  updateEditedTodo(todoText, todoId);
+});
+
 // ====================================================
 // =============        Funksiyalar        ============
 // ====================================================
@@ -45,6 +67,13 @@ const addTodoToState = (todoText) => {
   //   Draw UI
   drawUIByState();
 };
+const updateEditedTodo = (todoText, todoId) => {
+  const todoIndex = state.findIndex((elem) => elem.id === todoId);
+  state[todoIndex].text = todoText;
+  //   Draw UI
+  drawUIByState();
+  hideEditModal();
+};
 
 const deleteTodo = (id) => {
   // splice - dastlabki array o'zgartiradi
@@ -52,6 +81,11 @@ const deleteTodo = (id) => {
   state.splice(idx, 1);
   //   Draw UI
   drawUIByState();
+};
+
+const editTodo = (id) => {
+  const todo = state.find((elem) => elem.id === id);
+  showEditModal(todo.text, id);
 };
 
 const drawUIByState = () => {
@@ -63,6 +97,19 @@ const drawUIByState = () => {
 
   //   innerHTML hafli narsa
   todosWrapper.innerHTML = todosHTML;
+};
+
+const showEditModal = (todoText, todoId) => {
+  const safeTodoText = keepOnlyPlainTodoText(todoText);
+  editModal.style.display = "flex";
+  todoEditInput.value = safeTodoText;
+  todoEditInput.dataset.todoId = todoId;
+};
+
+const hideEditModal = () => {
+  editModal.style.display = "none";
+  todoEditInput.value = "";
+  delete todoEditInput.dataset.todoId;
 };
 
 // ====================================================
@@ -94,7 +141,7 @@ const makeOneTodoHtmlContent = (todo, todoNumber) => {
       <h3>${todoNumber}. ${safeTodoText}</h3>
     </div>
     <div class="one__todo__actions">
-      <button class="button__edit">Edit</button>
+      <button class="button__edit" onclick="editTodo(${todo.id})">Edit</button>
       <button class="button__delete" onclick="deleteTodo(${todo.id})">Delete</button>
     </div>
   </div>`;
